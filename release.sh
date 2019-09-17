5be113c49b0e2acf0258bb0973a7abbffcf0426a
yakuhzi <miguel@heidegger-ueberlingen.de>
2019-09-17 21:39:01 +0200

    changed release script

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 # Actions requires a node_modules dir https://github.com/actions/toolkit/blob/master/docs/javascript-action.md#publish-a-releasesv1-action
 # It is recommended not to check these in https://github.com/actions/toolkit/blob/master/docs/action-versioning.md#recommendations
 
-git checkout releases/v1 # If this branch already exists, omit the -b flag
+git checkout -b releases/v1
 rm -rf node_modules
 npm install --production
 git add -f node_modules
@@ -13,3 +13,4 @@ git push origin :refs/tags/v1
 git tag -fa v1 -m "Update v1 tag"
 git push origin v1
 git checkout master
+git branch -D releases/v1
