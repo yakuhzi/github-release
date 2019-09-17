a7ccdb0f9cfc519483d22fdf125cbae1f9ee9005
yakuhzi <miguel@heidegger-ueberlingen.de>
2019-09-17 20:50:41 +0200

    added licence

--- release.sh
+++ release.sh
@@ -2,14 +2,14 @@
 # Actions requires a node_modules dir https://github.com/actions/toolkit/blob/master/docs/javascript-action.md#publish-a-releasesv1-action
 # It is recommended not to check these in https://github.com/actions/toolkit/blob/master/docs/action-versioning.md#recommendations
 
-git checkout -b releases/v1 # If this branch already exists, omit the -b flag
+git checkout releases/v1 # If this branch already exists, omit the -b flag
 rm -rf node_modules
-sed -i '/node_modules/d' .gitignore # Bash command that removes node_modules from .gitignore
 npm install --production
-git add node_modules .gitignore
+git add -f node_modules
 git commit -m node_modules
 git push origin releases/v1
 
 git push origin :refs/tags/v1
 git tag -fa v1 -m "Update v1 tag"
 git push origin v1
+git checkout master
