18b3ab62e7e2ad4e422899b15ec79e8365154fd3
yakuhzi <miguel@heidegger-ueberlingen.de>
2025-12-18 19:36:47 +0100

    Chore: Update v4

---
+++ dist/release.js
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=release.js.map
---
+++ node_modules/@typescript-eslint/eslint-plugin/dist/rules/no-useless-default-assignment.d.ts
@@ -0,0 +1,3 @@
+type MessageId = 'uselessDefaultAssignment' | 'uselessUndefined';
+declare const _default: import("@typescript-eslint/utils/ts-eslint").RuleModule<MessageId, [], import("../../rules").ESLintPluginDocs, import("@typescript-eslint/utils/ts-eslint").RuleListener>;
+export default _default;
